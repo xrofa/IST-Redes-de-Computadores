68b2267197d0f04ee6535da489dbff910ae425cd
Christian Sousa <synvapors@gmail.com>
2017-10-09 18:16:02 +0100

    Pasta com os cliente-servidor tcp para enviar ficheiros

---
+++ fileserver_tcp/Client.java
@@ -0,0 +1,28 @@
+import java.net.*;
+import java.io.*;
+public class Client { 
+    public static void main (String [] args ) throws IOException { 
+        int filesize=1022386;
+        //String filename = args[0];
+        int bytesRead;
+        int currentTot = 0;
+        Socket socket = new Socket("127.0.0.1",15123);
+        byte [] bytearray = new byte [filesize];
+        InputStream is = socket.getInputStream();
+        FileOutputStream fos = new FileOutputStream("ficheiro_criado_cliente.txt");
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        bytesRead = is.read(bytearray,0,bytearray.length);
+        currentTot = bytesRead;
+        do { 
+            bytesRead = is.read(bytearray, currentTot, (bytearray.length-currentTot));
+            if(bytesRead >= 0)
+                currentTot += bytesRead;
+        } 
+        while(bytesRead > -1);
+        bos.write(bytearray, 0 , currentTot);
+        bos.flush();
+        bos.close();
+        socket.close();
+    } 
+} 
+
